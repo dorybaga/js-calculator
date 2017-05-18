b1882defe04504ffc17a75ebd95973f84df7cc13
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-17 21:44:43 -1000

    calculatorModule returns object literal

--- calculator.js
+++ calculator.js
@@ -11,64 +11,121 @@ var calculatorModule = function(){
   var memory = 0;
   var total = 0;
 
+  var calculator = {
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number } current total
    */
 
-
-
+  load: function(x){
+    total = x;
+    if (x === "number"){
+      return total;
+    } else{
+      throw error;
+    }
+  },
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+  getTotal: function(x){
+    return total;
+  },
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+  add: function(x){
+    if (x === "number"){
+      return total += x;
+    } else {
+      throw error;
+    }
+  },
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+   subtract: function(x){
+    if (x === "number"){
+      return total -= x;
+    } else {
+      throw error;
+    }
+   },
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
+   multiply: function(x){
+    if (x === "number"){
+      return total *= x;
+    } else {
+      throw error;
+    }
+   },
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   divide: function(x){
+    if (x === "number"){
+      return total /= x;
+    } else {
+      throw error;
+    }
+   },
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   recallMemory: function(x){
+    if (x === "number"){
+      return memory;
+    } else {
+      throw error;
+    }
+   },
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   saveMemory: function(x){
+    total = memory;
+    return memory;
+   },
 
   /**
    * Clear the value stored at `memory`
    */
 
+   clearMemory: function(x){
+    memory = 0;
+    return memory;
+   }
+
   /**
    * Validation
    */
 
+  };
 
-return calculatorModule;
+return calculator;
+
 };
+
+calculatorModule();
